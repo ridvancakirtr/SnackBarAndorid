8d93a543f47b5c32565df31e2b63066dddc25815
ridvancakirtr@gmail.com <MuhendisTurkiye48>
2018-05-18 03:08:51 +0300

    SnackBar Add

--- app/src/main/java/com/example/root/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/root/myapplication/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.root.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        btnShow.setOnClickListener {
+            val snackbar = Snackbar.make(constraintLayout,"Deneme", Snackbar.LENGTH_LONG)
+            snackbar.setAction("Close") {
+                snackbar.dismiss()
+            }
+            snackbar.setActionTextColor(ContextCompat.getColor(this,R.color.colorPrimary))
+            val view = snackbar.view
+            val text = view.findViewById<TextView>(android.support.design.R.id.snackbar_text)
+            text.setTextColor(ContextCompat.getColor(this,R.color.colorAccent))
+            snackbar.show()
+        }
     }
 }
